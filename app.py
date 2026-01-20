43918351a5b8ee4bea7368f2646ce7f00da57b12
maartuhaa <mitkevych2006@gmail.com>
2026-01-20 10:05:40 +0100

    Stage 1: base Python script created

---
+++ app.py
@@ -0,0 +1,9 @@
+def main():
+    print("Program is running...")
+    print("This feature will be protected by license later.")
+
+    input("Press Enter to exit...")
+
+
+if __name__ == "__main__":
+    main()
